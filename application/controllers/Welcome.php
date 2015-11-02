a953f96bf221456cf5d20afdde57624f7e4f310a
amorishi <a.kiyohito.m@hotmail.com>
2015-11-01 17:26:57 -0800

    added tutorial libraries and helpers content

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,8 +21,18 @@ class Welcome extends Application {
     function index()
     {
 	$this->data['pagebody'] = 'justone';    // this is the view we want shown
-	$this->data = array_merge($this->data, (array) $this->quotes->last());
-	$this->render();
+	//$this->data = array_merge($this->data, (array) $this->quotes->last());
+	// choose random quote
+    $choice = rand(1, $this->quotes->size());
+    $this->data = array_merge($this->data, (Array)$this->quotes->get($choice));
+    
+    $this->caboose->needed('jrating', 'hollywood');
+
+
+    $this->data['average'] = ($this->data['vote_count'] > 0) ?
+        ($this->data['vote_total'] / $this->data['vote_count']) : 0;
+
+    $this->render();
     }
 
 }
