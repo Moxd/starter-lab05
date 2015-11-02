215f10b96d0789834e405056a9ebfa90a1fc01eb
amorishi <a.kiyohito.m@hotmail.com>
2015-11-01 22:58:32 -0800

    Added adding new entries and validation, tutorial 5b

---
+++ application/views/admin_list.php
@@ -0,0 +1,17 @@
+<table cols="" border="0">
+	<tr>
+	</tr>
+		<th>ID</th>
+		<th>Author</th>
+		<th>Mugshot</th>
+		<th>Their Saying</th>
+	{quotes}
+	<tr>
+		<th>{id}</th>
+		<th>{who}</th>
+		<th>{mug}</th>
+		<th>{what}</th>
+	</tr>
+	{/quotes}
+</table>
+<a href="/admin/add">Add a new quotation</a>
